207d6f023f6ffd09c8900198dc4dcf31671db87e
muyeen <muyeenulislamsakif@gmail.com>
2025-04-18 23:29:30 +0600

    changes

---
+++ src/components/layout/navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center sticky top-0 right-0 w-full transition-all duration-300 z-[1000]">
+      Navbar
+    </div>
+  );
+};
+
+export default Navbar;
