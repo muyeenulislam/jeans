29cd5ea79fd819eb5c31ac583eac954285fde494
muyeen <muyeenulislamsakif@gmail.com>
2025-04-26 15:08:43 +0600

    og updates

--- src/app/contact/layout.js
+++ src/app/contact/layout.js
@@ -4,46 +4,14 @@ import DefaultLoader from "../../components/loader/defaultloader";
 
 export async function generateMetadata() {
   return {
-    author: "Emad Masoud",
     alternates: {
       canonical: "https://emadmasoud.com/contact",
     },
     openGraph: {
-      title: "CEO in Jeans – A Real-World Guide to Bold Leadership",
-      site_name: "CEO in Jeans – A Real-World Guide to Bold Leadership",
-      description:
-        "More than a business book, CEO in Jeans is a personal journey about leading with conviction, clarity, and no mask.",
       url: "https://emadmasoud.com/contact",
-      type: "website",
-      author: "Emad Masoud",
-      images: [
-        {
-          url: "/images/favicon-light.png",
-          width: 100,
-          height: 100,
-          alt: "jeans",
-        },
-      ],
     },
     twitter: {
-      title: "CEO in Jeans – A Real-World Guide to Bold Leadership",
-      description:
-        "More than a business book, CEO in Jeans is a personal journey about leading with conviction, clarity, and no mask.",
       url: "https://emadmasoud.com/contact",
-      type: "website",
-      author: "Emad Masoud",
-      images: [
-        {
-          url: "/images/favicon-light.png",
-          width: 100,
-          height: 100,
-          alt: "jeans",
-        },
-      ],
-    },
-    robots: {
-      index: true,
-      follow: true,
     },
   };
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -20,78 +20,81 @@ const ibm = IBM_Plex_Sans({
 });
 
 export const metadata = {
-  title:
-    "CEO in Jeans – A Real-World Guide to Bold Leadership & Human-Centered Decisions",
-  description:
-    "CEO in Jeans is not just a leadership book. It's a raw, real, and refreshing journey into decision-making, ambition, and leading with heart in a suit-free world. Written by Emad Masoud., it blends strategy, humility, and personal fire.",
-  author: "Emad Masoud",
-  keywords:
-    "leadership, CEO book, Emad N, business strategy, human-centered leadership, executive book, decision-making, entrepreneurship, real-world leadership",
-  type: "website",
+  alternates: {
+    canonical: "https://emadmasoud.com/",
+  },
   icons: [
     {
       rel: "icon",
       type: "icon",
       sizes: "32x32",
-      url: "/images/favicon-light.ico",
+      url: "./favicon.ico",
     },
     {
       rel: "icon",
       type: "image/png",
       sizes: "16x16",
-      url: "/images/favicon-light.ico",
+      url: "./favicon.ico",
     },
     {
       rel: "apple-touch-icon",
       sizes: "180x180",
-      url: "/images/favicon-light.ico",
+      url: "./favicon.ico",
     },
   ],
-  alternates: {
-    canonical: "https://emadmasoud.com",
-  },
-  openGraph: {
-    title: "CEO in Jeans – A Real-World Guide to Bold Leadership",
-    site_name: "CEO in Jeans – A Real-World Guide to Bold Leadership",
-    description:
-      "More than a business book, CEO in Jeans is a personal journey about leading with conviction, clarity, and no mask.",
-    url: "https://emadmasoud.com",
-    type: "website",
-    author: "Emad Masoud",
-    images: [
-      {
-        url: "/images/favicon-light.png",
-        width: 100,
-        height: 100,
-        alt: "jeans",
-      },
-    ],
-  },
-  twitter: {
-    title: "CEO in Jeans – A Real-World Guide to Bold Leadership",
-    description:
-      "More than a business book, CEO in Jeans is a personal journey about leading with conviction, clarity, and no mask.",
-    url: "https://emadmasoud.com",
-    type: "website",
-    author: "Emad Masoud",
-    images: [
-      {
-        url: "/images/favicon-light.png",
-        width: 100,
-        height: 100,
-        alt: "jeans",
-      },
-    ],
-  },
-  robots: {
-    index: true,
-    follow: true,
-  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>
+          CEO in Jeans – A Real-World Guide to Bold Leadership & Human-Centered
+          Decisions
+        </title>
+        <meta
+          name="description"
+          content="CEO in Jeans is not just a leadership book. It's a raw, real, and refreshing journey into decision-making, ambition, and leading with heart in a suit-free world. Written by Emad Masoud., it blends strategy, humility, and personal fire."
+        />
+        <meta
+          name="keywords"
+          content="leadership, CEO book, Emad N, business strategy, human-centered leadership, executive book, decision-making, entrepreneurship, real-world leadership"
+        />
+        <meta
+          property="og:title"
+          content="CEO in Jeans – A Real-World Guide to Bold Leadership"
+        />
+        <meta
+          property="og:description"
+          content="More than a business book, CEO in Jeans is a personal journey about leading with conviction, clarity, and no mask."
+        />
+        <meta property="og:url" content="https://emadmasoud.com" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:site_name"
+          content="CEO in Jeans – A Real-World Guide to Bold Leadership & Human-Centered
+          Decisions"
+        />
+        <meta property="og:image" content="/images/favicon-light.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="emadmasoud.com" />
+        <meta property="twitter:url" content="https://emadmasoud.com" />
+        <meta
+          property="twitter:site"
+          content="CEO in Jeans – A Real-World Guide to Bold Leadership & Human-Centered
+          Decisions"
+        />
+        <meta
+          name="twitter:title"
+          content="CEO in Jeans – A Real-World Guide to Bold Leadership"
+        />
+        <meta
+          name="twitter:description"
+          content="More than a business book, CEO in Jeans is a personal journey about leading with conviction, clarity, and no mask."
+        />
+        <meta name="twitter:image" content="/images/favicon-light.png" />
+        <meta name="robots" content="index, follow" />
+      </head>
       <ConfigProvider
         theme={{
           components: {},
